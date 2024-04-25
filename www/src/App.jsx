5648c3837e37d1e966ba439585851d1aca06c062
re[L]tro-kram <ortlermark@gmail.com>
2024-04-25 14:48:53 +0200

    fix

--- www/src/App.jsx
+++ www/src/App.jsx
@@ -11,5 +11,20 @@ import { MyDecoratedClass } from "@template/decorators";
 export function App() {
   const foo = new MyDecoratedClass();
 
-  return <div>Hello World</div>;
+  return (
+    <div
+      style={{
+        position: "absolute",
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Hello Deco-World
+    </div>
+  );
 }
